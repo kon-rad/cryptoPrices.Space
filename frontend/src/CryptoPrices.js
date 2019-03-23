23c4507e6265d059b6706b5cbb36008a7b9385c4
konradgnat <konradmgnat@gmail.com>
2019-03-23 18:29:58 -0500

    allowed

--- frontend/src/CryptoPrices.js
+++ frontend/src/CryptoPrices.js
@@ -3,14 +3,12 @@ let API_URL = '';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     API_URL = 'http://localhost:8085';
 }
-console.log(API_URL, process.env.NODE_ENV);
 
 export default class CryptoPrices{
 
     constructor(){}
     
     getPrices() {
-        console.log('API_URL -> ', API_URL);
         const url = `${API_URL}/api/prices/`;
         return axios.get(url).then(response => response.data);
     }
